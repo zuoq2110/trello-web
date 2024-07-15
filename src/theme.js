26e7e12a49d47162c42d3ffa3c1c7bf0c30c00f0
LAPTOP-2H1VKPL9\Hp <zuoq2110@gmail.com>
2024-07-15 22:44:12 +0700

    add theme

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles'
+import { green, purple } from '@mui/material/colors'
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: purple[500]
+    },
+    secondary: {
+      main: green[500]
+    }
+  }
+})
+
+export default theme
