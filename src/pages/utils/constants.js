b92af2fe795846ad06da3890d1686852ce828da7
LAPTOP-2H1VKPL9\Hp <zuoq2110@gmail.com>
2024-10-26 10:27:33 +0700

    fix build_mode

--- src/pages/utils/constants.js
+++ src/pages/utils/constants.js
@@ -1,2 +1,8 @@
-export const API_ROOT = 'http://localhost:8017'
-// export const API_ROOT = 'https://trello-api-inj2.onrender.com'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-inj2.onrender.com'
+}
+export const API_ROOT = apiRoot
